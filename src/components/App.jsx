3b97bd6b03a7e4f55a72b5395b3c19f247481bca
DARSHAN SOJITRA <143096142+Darshasojitra@users.noreply.github.com>
2026-06-04 12:03:17 +0000

    hashrouter added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Body";
 import Home from "./Home";
 import About from "./About";
@@ -11,23 +11,33 @@ import Career from "./Career";
 const App = () => {
   return (
     <>
-      <BrowserRouter basename="/">
+      {/* <BrowserRouter basename="/"> */}
+       <HashRouter>
         <Routes>
           <Route path="/" element={<Body />}>
-            <Route path="/" element={<Home />}/>
+            {/* <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
             <Route path="/services" element={<Services />}/>
             <Route path="/portfolio" element={<Portfolio />}/>
             <Route path="/blogs" element={<Blogs />}/>
             <Route path="/contactus" element={<ContactUs />}/>
-            <Route path="/career" element={<Career />}/>
+            <Route path="/career" element={<Career />}/> */}
+              <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contactus" element={<ContactUs />} />
+          <Route path="career" element={<Career />} />
+          
           </Route>
-        </Routes>
+        {/* </Routes>
         
-        <Routes>
+        <Routes> */}
           <Route path="/dashboard" element={<div>dashboard</div>}/>
         </Routes>
-      </BrowserRouter>
+        </HashRouter>
+      {/* </BrowserRouter> */}
     </>
   );
 };
